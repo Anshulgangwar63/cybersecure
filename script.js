191b535fb3278b96f1263cb9f4e049a2f4b11a36
Anshul Gangwar <anshulgangwar63@gmail.com>
2019-04-02 18:25:47 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
    $(window).on('load', function() {
    $('.preloader').fadeOut('slow');
     })
+
+$(function() {
+    //caches a jQuery object containing the header element
+    var header = $(".MyNav");
+    $(window).scroll(function() {
+        var scroll = $(window).scrollTop();
+
+        if (scroll >= 100) {
+            header.removeClass('nav-transparent').addClass("nav-colored");
+        } else {
+            header.removeClass("nav-colored").addClass('nav-transparent');
+        }
+    });
+});
